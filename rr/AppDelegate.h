732d292e5c0da38055365d0ea3d50f8e8f5e2648
michael carson <michaelcarson@asc-toshiba2.gateway.2wire.net>
2012-01-26 20:46:00 -0800

    Initial Commit

---
+++ rr/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  rr
+//
+//  Created by michael carson on 1/26/12.
+//  Copyright (c) 2012 coolfone. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ rr/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  rr
+//
+//  Created by michael carson on 1/26/12.
+//  Copyright (c) 2012 coolfone. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
