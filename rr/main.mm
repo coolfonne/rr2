285ec3a678c95db54f27a92537b55bcf6512ee31
m <michael.carson@mail.com>
2013-08-20 21:39:12 -0500

    aa

--- rr/main.mm
+++ rr/main.mm
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  rr
-//
-//  Created by michael carson on 1/26/12.
-//  Copyright (c) 2012 coolfone. All rights reserved.
-//
-
 #import <Cocoa/Cocoa.h>
 #include <GLUT/glut.h>
 #include "kdtree.h"
